b4a05f11fade69c1ccfcc073eb4eebea4d426e74
alescetto03 <alescetto03@gmail.com>
2026-01-18 21:12:41 +0100

    modifiche ai test

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,35 +1,45 @@
-import { test, expect } from '@playwright/test'
+import { test, expect, Page } from '@playwright/test'
 
 //TODO::Includere un before each che controlla se il file .env è presente, sennò stampa un messaggio di errore e interrompe qualsiasi test
 
-test('should login with correct credentials', async ({ page }) => {
-  await page.goto('http://localhost:4200/')
+const env_entries = [
+  "E2E_USERNAME",
+  "E2E_PASSWORD",
+  "BASE_HOST"
+]
+
+test.beforeAll('check if there is env file', async () => {
+  const missing = env_entries.filter(e => !process.env[e]);
+
+  if (missing.length > 0) {
+    throw new Error(`Variabili d'ambiente mancanti: ${missing.join(', ')}`);
+  }
+});
+
+async function authenticateUser(page: Page, host: string, user: string, password: string) {
+  await page.goto(host)
   await page.locator('header').getByRole('link', { name: 'Accedi' }).click()
-  await page.getByPlaceholder('username').fill(process.env.E2E_USERNAME!)
-  await page.getByPlaceholder('password').fill(process.env.E2E_PASSWORD!)
+  await page.getByPlaceholder('username').fill(user)
+  await page.getByPlaceholder('password').fill(password)
   await page.getByRole('button', { name: 'Accedi' }).click()
+}
+
+test('should login with correct credentials', async ({ page }) => {
+  authenticateUser(page, process.env.BASE_HOST!, process.env.E2E_USERNAME!, process.env.E2E_PASSWORD!)
   const header = page.locator('header')
   await expect(header).toBeVisible()
   await expect(header).toContainText(`Benvenuto, ${process.env.E2E_USERNAME!}`)
 })
 
 test('should prevent login with incorrect credentials', async ({ page }) => {
-  await page.goto('http://localhost:4200/')
-  await page.locator('header').getByRole('link', { name: 'Accedi' }).click()
-  await page.getByPlaceholder('username').fill(process.env.E2E_USERNAME!)
-  await page.getByPlaceholder('password').fill("randompass")
-  await page.getByRole('button', { name: 'Accedi' }).click()
+  authenticateUser(page, process.env.BASE_HOST!, process.env.E2E_USERNAME!, "randompass")
   const toast = page.locator('#toast-container')
   await expect(toast).toBeVisible()
   await expect(toast).toContainText("Oops! Credenziali non valide")
 })
 
 test('should log out correctly', async ({ page }) => {
-  await page.goto('http://localhost:4200/')
-  await page.locator('header').getByRole('link', { name: 'Accedi' }).click()
-  await page.getByPlaceholder('username').fill(process.env.E2E_USERNAME!)
-  await page.getByPlaceholder('password').fill(process.env.E2E_PASSWORD!)
-  await page.getByRole('button', { name: 'Accedi' }).click()
+  authenticateUser(page, process.env.BASE_HOST!, process.env.E2E_USERNAME!, process.env.E2E_PASSWORD!)
   const header = page.locator('header')
   await expect(header).toBeVisible()
   await expect(header).toContainText(`Benvenuto, ${process.env.E2E_USERNAME!}`)
@@ -39,10 +49,10 @@ test('should log out correctly', async ({ page }) => {
   await expect(toast).toContainText(`Torna presto, ${process.env.E2E_USERNAME!}!`)
 })
 
-const user = "testuser"
 
 test('should register user with correct credentials', async ({ page }) => {
-  await page.goto('http://localhost:4200/')
+const user = "newtestuser"
+  await page.goto(process.env.BASE_HOST!)
   await page.locator('header').getByRole('link', { name: 'Accedi' }).click()
   await page.locator('app-authentication-page').getByRole('link', { name: 'Registrati' }).click()
   await page.getByPlaceholder('username').fill(user)
@@ -54,7 +64,7 @@ test('should register user with correct credentials', async ({ page }) => {
 })
 
 test('should prevent registering user with empty fields', async ({ page }) => {
-  await page.goto('http://localhost:4200/')
+  await page.goto(process.env.BASE_HOST!)
   await page.locator('header').getByRole('link', { name: 'Accedi' }).click()
   await page.locator('app-authentication-page').getByRole('link', { name: 'Registrati' }).click()
   await page.getByRole('button', { name: 'Registrati' }).click()
@@ -72,7 +82,7 @@ test('should prevent registering user with empty fields', async ({ page }) => {
 })
 
 test('should display message user already exists', async ({ page }) => {
-  await page.goto('http://localhost:4200/')
+  await page.goto(process.env.BASE_HOST!)
   await page.locator('header').getByRole('link', { name: 'Accedi' }).click()
   await page.locator('app-authentication-page').getByRole('link', { name: 'Registrati' }).click()
   await page.getByPlaceholder('username').fill(user)
@@ -84,7 +94,7 @@ test('should display message user already exists', async ({ page }) => {
 })
 
 test('should register cat correctly', async ({ page }) => {
-  await page.goto('http://localhost:4200/')
+  await page.goto(process.env.BASE_HOST!)
   await page.locator('header').getByRole('link', { name: 'Accedi' }).click()
   await page.getByPlaceholder('username').fill(user)
   await page.getByPlaceholder('password').fill(process.env.E2E_PASSWORD!)
@@ -117,7 +127,7 @@ test('should register cat correctly', async ({ page }) => {
 })
 
 test('should prevent registering cat with empty fields', async ({ page }) => {
-  await page.goto('http://localhost:4200/')
+  await page.goto(process.env.BASE_HOST!)
   await page.locator('header').getByRole('link', { name: 'Accedi' }).click()
   await page.getByPlaceholder('username').fill(user)
   await page.getByPlaceholder('password').fill(process.env.E2E_PASSWORD!)
@@ -131,26 +141,26 @@ test('should prevent registering cat with empty fields', async ({ page }) => {
   const form = page.locator('app-cat-form form');
   await expect(
     page.getByText('Inserisci un titolo.')
-  ).toBeVisible();
+  ).toBeVisible()
   await expect(
     page.getByText("Inserisci un'immagine.")
-  ).toBeVisible();
+  ).toBeVisible()
     await expect(
     page.getByText('Inserisci una descrizione.')
-  ).toBeVisible();
+  ).toBeVisible()
   await expect(
     page.getByText('Clicca sulla mappa per inserire la posizione del tuo randagio.')
-  ).toBeVisible();
+  ).toBeVisible() 
 })
 
 test('should display file error message', async ({ page }) => {
-  await page.goto('http://localhost:4200/')
+  await page.goto(process.env.BASE_HOST!)
   await page.locator('header').getByRole('link', { name: 'Accedi' }).click()
-  await page.getByPlaceholder('username').fill(user)
+  await page.getByPlaceholder('username').fill("admin")
   await page.getByPlaceholder('password').fill(process.env.E2E_PASSWORD!)
   await page.getByRole('button', { name: 'Accedi' }).click()
   await page.locator('header').getByRole('link', { name: 'Inserisci un randagio' }).click()
-  await page.locator('input[type="file"]').setInputFiles('tests/images/cat.jpeg')
+  await page.locator('input[type="file"]').setInputFiles('tests/images/mushroom.png')
   await page.getByPlaceholder('Inserisci un titolo...').fill("Gatto di test")
   await page.getByPlaceholder('Scrivi qui in markdown...').fill("Test prova")
   await page.locator('#catFormMap').click()  
@@ -164,7 +174,7 @@ test('should display file error message', async ({ page }) => {
 })
 
 test('should display new comment', async ({ page }) => {
-  await page.goto('http://localhost:4200/')
+  await page.goto(process.env.BASE_HOST!)
   await page.locator('header').getByRole('link', { name: 'Accedi' }).click()
   await page.getByPlaceholder('username').fill(user)
   await page.getByPlaceholder('password').fill(process.env.E2E_PASSWORD!)
